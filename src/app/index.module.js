26e97e115343ceebee5da35765e4519c27e55e8c
Nghia Tran <nghiattran3@gmail.com>
2016-12-04 14:19:18 -0500

    init

---
+++ src/app/index.module.js
@@ -0,0 +1,7 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('nproject3', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngAria', 'ui.router', 'ui.bootstrap', 'toastr']);
+
+})();
